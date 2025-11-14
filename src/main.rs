e6155c6a264eb31225758085f297d83ea34e2d0f
Peter Arato <it.arato@gmail.com>
2025-11-13 21:02:57 -0500

    Loop

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,17 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    let mut buf = String::new();
-    io::stdin()
-        .read_line(&mut buf)
-        .expect("Failed reading STDIN");
+        let mut buf = String::new();
+        io::stdin()
+            .read_line(&mut buf)
+            .expect("Failed reading STDIN");
 
-    println!("{}: command not found", buf.trim());
+        println!("{}: command not found", buf.trim());
 
-    io::stdout().flush().unwrap();
+        io::stdout().flush().unwrap();
+    }
 }
