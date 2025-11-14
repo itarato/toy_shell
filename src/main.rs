f572102563aa06e4ef9591b6b7abb08619a181f3
Peter Arato <it.arato@gmail.com>
2025-11-13 21:00:35 -0500

    Error output

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,13 @@ use std::io::{self, Write};
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut buf = String::new();
+    io::stdin()
+        .read_line(&mut buf)
+        .expect("Failed reading STDIN");
+
+    println!("{}: command not found", buf.trim());
+
+    io::stdout().flush().unwrap();
 }
