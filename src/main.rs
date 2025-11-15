a61216c8cde99c26644b891eecfc60be9f2c9be5
Peter Arato <it.arato@gmail.com>
2025-11-15 14:05:20 -0500

    Refactor.

--- src/main.rs
+++ src/main.rs
@@ -25,102 +25,142 @@ enum CommandParseState {
     DoubleQuoteSection,
 }
 
-fn split_command(raw: &str) -> Option<(String, Vec<String>)> {
-    let mut state: CommandParseState = CommandParseState::DropSection;
-    let mut parts: Vec<String> = vec![];
+struct ArgParser {
+    chars: Vec<char>,
+    i: usize,
+    buf: String,
+}
 
-    let chars = raw.chars().collect::<Vec<_>>();
-    let mut i = 0;
-    let mut buf = String::new();
-
-    while i < chars.len() {
-        let c = chars[i];
-
-        match state {
-            CommandParseState::DropSection => {
-                if c.is_whitespace() {
-                    // Noop.
-                } else if c == '\'' {
-                    state = CommandParseState::SingleQuoteSection;
-                } else if c == '"' {
-                    state = CommandParseState::DoubleQuoteSection;
-                } else {
-                    state = CommandParseState::WordSection;
-                    buf.push(c);
-                }
-            }
-            CommandParseState::SingleQuoteSection => {
-                if c == '\'' {
-                    if chars.len() > i + 1 && !chars[i + 1].is_whitespace() {
-                        if chars[i + 1] == '\'' {
-                            i += 1;
-                        } else if chars[i + 1] == '"' {
-                            i += 1;
-                            state = CommandParseState::DoubleQuoteSection;
-                        } else {
-                            state = CommandParseState::WordSection;
-                        }
-                    } else {
-                        state = CommandParseState::DropSection;
-                        parts.push(buf.clone());
-                        buf.clear();
-                    }
-                } else {
-                    buf.push(c);
-                }
-            }
-            CommandParseState::DoubleQuoteSection => {
-                if c == '"' {
-                    if chars.len() > i + 1 && !chars[i + 1].is_whitespace() {
-                        if chars[i + 1] == '\'' {
-                            i += 1;
-                            state = CommandParseState::SingleQuoteSection;
-                        } else if chars[i + 1] == '"' {
-                            i += 1;
-                        } else {
-                            state = CommandParseState::WordSection;
-                        }
-                    } else {
-                        state = CommandParseState::DropSection;
-                        parts.push(buf.clone());
-                        buf.clear();
-                    }
-                } else {
-                    buf.push(c);
-                }
-            }
-            CommandParseState::WordSection => {
-                if c.is_whitespace() {
-                    state = CommandParseState::DropSection;
-                    parts.push(buf.clone());
-                    buf.clear();
-                } else if c == '\'' {
-                    state = CommandParseState::SingleQuoteSection;
-                } else if c == '"' {
-                    state = CommandParseState::DoubleQuoteSection;
-                } else {
-                    buf.push(c);
-                }
-            }
-        };
-
-        i += 1;
+impl ArgParser {
+    fn new(raw: &str) -> Self {
+        Self {
+            chars: raw.chars().collect(),
+            i: 0,
+            buf: String::new(),
+        }
     }
 
-    if let CommandParseState::WordSection = state {
-        parts.push(buf.clone());
+    fn at_end(&self) -> bool {
+        self.i >= self.chars.len()
     }
 
-    if parts.len() < 1 {
-        None
-    } else {
-        let name = parts.remove(0);
-        Some((name, parts))
+    fn current(&self) -> char {
+        self.chars[self.i]
+    }
+
+    fn has_n_more(&self, n: usize) -> bool {
+        self.chars.len() > self.i + n
+    }
+
+    fn peek(&self) -> char {
+        self.peekn(1)
+    }
+
+    fn peekn(&self, n: usize) -> char {
+        self.chars[self.i + n]
+    }
+
+    fn next(&mut self) {
+        self.i += 1;
+    }
+
+    fn push(&mut self) {
+        self.buf.push(self.current());
+    }
+
+    fn parse(mut self) -> Option<(String, Vec<String>)> {
+        let mut state: CommandParseState = CommandParseState::DropSection;
+        let mut parts: Vec<String> = vec![];
+
+        while !self.at_end() {
+            let c = self.current();
+
+            match state {
+                CommandParseState::DropSection => {
+                    if c.is_whitespace() {
+                        // Noop.
+                    } else if c == '\'' {
+                        state = CommandParseState::SingleQuoteSection;
+                    } else if c == '"' {
+                        state = CommandParseState::DoubleQuoteSection;
+                    } else {
+                        state = CommandParseState::WordSection;
+                        self.push();
+                    }
+                }
+                CommandParseState::SingleQuoteSection => {
+                    if c == '\'' {
+                        if self.has_n_more(1) && !self.peek().is_whitespace() {
+                            if self.peek() == '\'' {
+                                self.next();
+                            } else if self.peek() == '"' {
+                                self.next();
+                                state = CommandParseState::DoubleQuoteSection;
+                            } else {
+                                state = CommandParseState::WordSection;
+                            }
+                        } else {
+                            state = CommandParseState::DropSection;
+                            parts.push(self.buf.clone());
+                            self.buf.clear();
+                        }
+                    } else {
+                        self.push();
+                    }
+                }
+                CommandParseState::DoubleQuoteSection => {
+                    if c == '"' {
+                        if self.has_n_more(1) && !self.peek().is_whitespace() {
+                            if self.peek() == '\'' {
+                                self.next();
+                                state = CommandParseState::SingleQuoteSection;
+                            } else if self.peek() == '"' {
+                                self.next();
+                            } else {
+                                state = CommandParseState::WordSection;
+                            }
+                        } else {
+                            state = CommandParseState::DropSection;
+                            parts.push(self.buf.clone());
+                            self.buf.clear();
+                        }
+                    } else {
+                        self.push();
+                    }
+                }
+                CommandParseState::WordSection => {
+                    if c.is_whitespace() {
+                        state = CommandParseState::DropSection;
+                        parts.push(self.buf.clone());
+                        self.buf.clear();
+                    } else if c == '\'' {
+                        state = CommandParseState::SingleQuoteSection;
+                    } else if c == '"' {
+                        state = CommandParseState::DoubleQuoteSection;
+                    } else {
+                        self.push();
+                    }
+                }
+            };
+
+            self.next();
+        }
+
+        if let CommandParseState::WordSection = state {
+            parts.push(self.buf.clone());
+        }
+
+        if parts.len() < 1 {
+            None
+        } else {
+            let name = parts.remove(0);
+            Some((name, parts))
+        }
     }
 }
 
 fn parse_command(raw: &str) -> Command {
-    let (name, args) = match split_command(raw) {
+    let (name, args) = match ArgParser::new(raw).parse() {
         Some(name_args_pair) => name_args_pair,
         None => return Command::Invalid,
     };
