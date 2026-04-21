a9560f3496101840b8d4105b7b27938667208331
Peter Arato <it.arato@gmail.com>
2026-04-21 17:52:30 -0400

    Add complete set

--- src/common.rs
+++ src/common.rs
@@ -1,3 +1,318 @@
+use crate::arg_parser::*;
+use crate::command::*;
+use crate::completion::*;
+use crate::redirect::*;
+use is_executable::IsExecutable;
+use rustyline::{
+    history::{DefaultHistory, History},
+    Editor,
+};
+#[allow(unused_imports)]
+use std::io::{self, Write};
+use std::{
+    fs,
+    io::BufRead,
+    path::{Path, PathBuf},
+    process::Child,
+};
+
 pub(crate) const SHELL_BUILTIN_COMMANDS: [&'static str; 8] = [
     "echo", "type", "exit", "pwd", "cd", "history", "jobs", "complete",
 ];
+
+#[derive(Clone, Debug)]
+pub(crate) struct CommandWithContext {
+    pub(crate) original: String,
+    pub(crate) cmd: Command,
+    pub(crate) is_job: bool,
+    pub(crate) stdout_redirect: MaybeRedirect,
+    pub(crate) stderr_redirect: MaybeRedirect,
+}
+
+#[derive(Debug)]
+pub(crate) struct PipedCommands(pub(crate) Vec<CommandWithContext>);
+
+impl PipedCommands {
+    fn new_invalid() -> Self {
+        Self(vec![CommandWithContext {
+            original: String::new(),
+            cmd: Command::Invalid,
+            is_job: false,
+            stdout_redirect: None,
+            stderr_redirect: None,
+        }])
+    }
+}
+
+pub(crate) fn parse_command(raw: &str) -> PipedCommands {
+    let mut raw_cmds = match ArgParser::new(raw).parse() {
+        Some(v) => v.0,
+        None => {
+            return PipedCommands(vec![CommandWithContext {
+                original: String::new(),
+                cmd: Command::Invalid,
+                is_job: false,
+                stdout_redirect: None,
+                stderr_redirect: None,
+            }])
+        }
+    };
+
+    let mut cmds_with_context = vec![];
+    while !raw_cmds.is_empty() {
+        let raw_cmd = raw_cmds.remove(0);
+        let cmd = if raw_cmd.name == "exit" {
+            let exit_code = if raw_cmd.args.len() == 1 {
+                if let Ok(v) = i32::from_str_radix(&raw_cmd.args[0], 10) {
+                    v
+                } else {
+                    return PipedCommands::new_invalid();
+                }
+            } else if raw_cmd.args.len() > 1 {
+                return PipedCommands::new_invalid();
+            } else {
+                0
+            };
+            Command::Exit(exit_code)
+        } else if raw_cmd.name == "echo" {
+            Command::Echo(raw_cmd.args)
+        } else if raw_cmd.name == "type" {
+            if raw_cmd.args.len() != 1 {
+                Command::Invalid
+            } else {
+                Command::Type(raw_cmd.args[0].clone())
+            }
+        } else if raw_cmd.name == "pwd" {
+            Command::Pwd
+        } else if raw_cmd.name == "history" {
+            if raw_cmd.args.is_empty() {
+                Command::History(usize::MAX)
+            } else if raw_cmd.args.len() == 1 {
+                match usize::from_str_radix(&raw_cmd.args[0], 10) {
+                    Ok(v) => Command::History(v),
+                    Err(_) => return PipedCommands::new_invalid(),
+                }
+            } else if raw_cmd.args.len() == 2 && raw_cmd.args[0] == "-r" {
+                Command::HistoryAppend(raw_cmd.args[1].clone().into())
+            } else if raw_cmd.args.len() == 2 && raw_cmd.args[0] == "-w" {
+                Command::HistorySave(raw_cmd.args[1].clone().into(), false)
+            } else if raw_cmd.args.len() == 2 && raw_cmd.args[0] == "-a" {
+                Command::HistorySave(raw_cmd.args[1].clone().into(), true)
+            } else {
+                return PipedCommands::new_invalid();
+            }
+        } else if raw_cmd.name == "cd" {
+            if raw_cmd.args.len() != 1 {
+                Command::Invalid
+            } else {
+                Command::Cd(raw_cmd.args[0].clone())
+            }
+        } else if raw_cmd.name == "jobs" {
+            Command::Jobs
+        } else if raw_cmd.name == "complete" {
+            if raw_cmd.args.len() == 2 && raw_cmd.args[0] == "-p" {
+                Command::CompleteGet(raw_cmd.args[1].clone())
+            } else if raw_cmd.args.len() == 3 && raw_cmd.args[0] == "-C" {
+                let script_path = raw_cmd.args[1].clone();
+                let command = raw_cmd.args[2].clone();
+                Command::CompleteSet {
+                    script_path,
+                    command,
+                }
+            } else {
+                Command::Invalid
+            }
+        } else if raw_cmd.name.is_empty() {
+            Command::Empty
+        } else {
+            Command::Unknown(raw_cmd.name, raw_cmd.args)
+        };
+
+        cmds_with_context.push(CommandWithContext {
+            original: raw_cmd.original,
+            cmd,
+            is_job: raw_cmd.is_job,
+            stdout_redirect: raw_cmd.stdout_redirect,
+            stderr_redirect: raw_cmd.stderr_redirect,
+        });
+    }
+
+    PipedCommands(cmds_with_context)
+}
+
+pub(crate) fn verify_executable(name: &str, env_paths: &Vec<PathBuf>) -> Option<String> {
+    for env_path in env_paths {
+        let path = Path::new(&env_path).join(name);
+        if let Ok(true) = std::fs::exists(&path) {
+            if path.is_executable() {
+                return Some(path.to_str().unwrap().into());
+            }
+        }
+    }
+
+    None
+}
+
+pub(crate) fn home_path_expand(path: String) -> String {
+    if path == "~" {
+        std::env::home_dir()
+            .expect("Failed getting home dir")
+            .to_str()
+            .expect("Failed to convert to string")
+            .into()
+    } else if path.starts_with("~/") {
+        std::env::home_dir()
+            .expect("Failed getting home dir")
+            .join(&path[2..])
+            .to_str()
+            .expect("Failed to convert to string")
+            .into()
+    } else {
+        path
+    }
+}
+
+pub(crate) fn output(
+    to_stdout: String,
+    stdout_redirect: MaybeRedirect,
+    maybe_pipe_writer: Option<io::PipeWriter>,
+) {
+    if let Some(mut pipe_writer) = maybe_pipe_writer {
+        pipe_writer.write_all(to_stdout.as_bytes()).unwrap();
+        pipe_writer.write_all(b"\n").unwrap();
+    } else if let Some(redirect) = stdout_redirect {
+        if let Ok(mut f) = redirect.file() {
+            if !to_stdout.is_empty() {
+                f.write_all(to_stdout.as_bytes()).unwrap();
+                f.write_all(b"\n").unwrap();
+            }
+        } else {
+            panic!("File was expected to exist");
+        }
+    } else {
+        if !to_stdout.is_empty() {
+            println!("{}", to_stdout);
+        }
+    }
+}
+
+pub(crate) fn output_error(to_stderr: String, stderr_redirect: MaybeRedirect) {
+    if let Some(redirect) = stderr_redirect {
+        if let Ok(mut f) = redirect.file() {
+            if !to_stderr.is_empty() {
+                f.write_all(to_stderr.as_bytes()).unwrap();
+                f.write_all(b"\n").unwrap();
+            }
+        } else {
+            panic!("File was expected to exist");
+        }
+    } else {
+        if !to_stderr.is_empty() {
+            eprintln!("{}", to_stderr);
+        }
+    }
+}
+
+pub(crate) fn verify_redirect_exist(maybe_redirect: &MaybeRedirect, original_cmd: &str) -> bool {
+    if let Some(redirect) = maybe_redirect {
+        if let Ok(_) = redirect.file() {
+            true
+        } else {
+            eprintln!(
+                "{}: {}: No such file or directory",
+                original_cmd, redirect.filename
+            );
+            false
+        }
+    } else {
+        true
+    }
+}
+
+pub(crate) fn preload_exec_names(env_paths: &Vec<PathBuf>) -> Vec<String> {
+    let mut out = vec![];
+    for path in env_paths {
+        let Ok(entries) = std::fs::read_dir(path) else {
+            continue;
+        };
+
+        for entry in entries.flatten() {
+            let Ok(metadata) = entry.metadata() else {
+                continue;
+            };
+
+            if !metadata.is_file() {
+                continue;
+            };
+
+            let filename_os = entry.file_name();
+            let Some(filename) = filename_os.to_str() else {
+                continue;
+            };
+
+            out.push(filename.to_string());
+        }
+    }
+
+    out
+}
+
+pub(crate) enum ExecutionResult {
+    None,
+    Child {
+        child: Child,
+    },
+    ChildWithOutputHandling {
+        child: Child,
+        stdout_redirect: Option<Redirect>,
+        stderr_redirect: Option<Redirect>,
+    },
+    ChildForBackground {
+        child: Child,
+        original: String,
+    },
+}
+
+pub(crate) fn append_to_history(
+    path: String,
+    rl: &mut Editor<BinaryAndFileCompleter, DefaultHistory>,
+) {
+    let f = fs::File::open(path).unwrap();
+    for line in io::BufReader::new(f).lines() {
+        if let Ok(line) = line {
+            if !line.is_empty() {
+                rl.add_history_entry(line).unwrap();
+            }
+        }
+    }
+}
+
+pub(crate) fn save_history(
+    path: &String,
+    rl: &mut Editor<BinaryAndFileCompleter, DefaultHistory>,
+    should_append: bool,
+    last_history_save_index: &mut usize,
+) {
+    let mut f = fs::OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(!should_append)
+        .append(should_append)
+        .open(path)
+        .unwrap();
+
+    let start_index = if should_append {
+        *last_history_save_index
+    } else {
+        0
+    };
+
+    for line in rl.history().iter().skip(start_index) {
+        f.write_all(line.as_bytes()).unwrap();
+        f.write_all(b"\n").unwrap();
+    }
+
+    if should_append {
+        *last_history_save_index = rl.history().len();
+    }
+}
